aa48f1aa2531f926a6a75883a7e0f4ae3a9dee6b
ezequiel <ezebm19@gmail.com>
2019-08-07 10:02:23 -0300

    WIP: abm clinicas

---
+++ src/components/clinicas/clinicas.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+     
+    </div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			Clinicas: [
+				{ id: 1, name: "Borda 1", beds_available:100, permission: "123213-22" },
+				{ id: 2, name: "Borda 2", beds_available:100, permission: "123213-22" },
+				{ id: 3, name: "Borda 3", beds_available:100, permission: "123213-22" },
+				{ id: 4, name: "Borda 4", beds_available:100, permission: "123213-22" }
+			]
+		}
+	}
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import clinica from 'vue'
 
 Vue.use(Router)
 const router = new Router({
-	router: []
+    router: [{
+     path: 'clinica',
+     name: 'clinica',
+     component: clinica
+   }]
 })
 
-export default router
+export default router;
