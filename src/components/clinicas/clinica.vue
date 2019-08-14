de9aa72330a842b0f9744548b1aed77b285a6b88
ezequiel <ezebm19@gmail.com>
2019-08-14 16:45:36 -0300

    nueva internacion

--- src/components/clinicas/clinica.vue
+++ src/components/clinicas/clinica.vue
@@ -1,97 +1,111 @@
 <template>
-  <div class="container-wrapper">
-		<el-header>
-			<div class="main-title">Clinica {{ clinica.name }}</div>
-			<div class="main-controls">
-				<el-button type="primary" @click="openModal()">Ingresar Paciente</el-button>
-			</div>
-		</el-header>
-		<el-main>
-		  <h3> id: {{ clinica.id }} </h3>
-		  <h3> camas disponibles: {{ clinica.beds_available }}</h3>
-		  <h3> permission: {{ clinica.permission }} </h3>
-		  <h3>pacientes</h3>
-		  <el-table
-	      :data="clinica.pacientes"
-	      style="width: 100%">
-	      <el-table-column
-	        prop="id"
-	        label="#">
-	      </el-table-column>
-	      <el-table-column
-	        prop="name"
-	        label="Nombre">
-	      </el-table-column>
-	      <el-table-column
-	        prop="last_name"
-	        label="apellido">
-	      </el-table-column>
-	       <el-table-column
-	        prop="dni"
-	        label="dni">
-	      </el-table-column>
-	       <el-table-column
-	        prop="numero_de_paciente"
-	        label="numero de paciente">
-	      </el-table-column>
-	       <el-table-column>
-	       	<template slot-scope="scope">
-	       		<router-link :to="{ name: 'Paciente', params: { id: scope.row.id } }">Ver</router-link>
-	       	</template>
-	      </el-table-column>
-	    </el-table>
-			{{ dialogFormVisible }}
-			<el-dialog title="Shipping address" :visible.sync="dialogFormVisible">
-				<el-form :model="newEntry" label-width="120px">
-					<el-form-item label="Promotion name">
-						<el-input v-model="newEntry.name"></el-input>
-					</el-form-item>
-					<el-form-item label="Zones">
-						<el-select v-model="newEntry.region" style="width: 100%;">
-							<el-option label="Zone No.1" value="shanghai"></el-option>
-							<el-option label="Zone No.2" value="beijing"></el-option>
-						</el-select>
-					</el-form-item>
-				</el-form>
-				<span slot="footer" class="dialog-footer">
-					<el-button @click="dialogFormVisible = false">Cancel</el-button>
-					<el-button type="primary" @click="dialogFormVisible = false">Confirm</el-button>
-				</span>
-			</el-dialog>
-		</el-main>
-  </div>
+    <div class="container-wrapper">
+        <el-header>
+            <div class="main-title">Clinica {{ clinica.name }}</div>
+            <div class="main-controls">
+                <el-button type="primary" @click="openModal()">Ingresar Paciente</el-button>
+            </div>
+        </el-header>
+        <el-main>
+            <h3> id: {{ clinica.id }} </h3>
+            <h3> camas disponibles: {{ clinica.beds_available }}</h3>
+            <h3> permission: {{ clinica.permission }} </h3>
+            <h3>pacientes</h3>
+            <el-table :data="clinica.pacientes" style="width: 100%">
+                <el-table-column prop="id" label="#">
+                </el-table-column>
+                <el-table-column prop="name" label="Nombre">
+                </el-table-column>
+                <el-table-column prop="last_name" label="apellido">
+                </el-table-column>
+                <el-table-column prop="dni" label="dni">
+                </el-table-column>
+                <el-table-column prop="numero_de_paciente" label="numero de paciente">
+                </el-table-column>
+                <el-table-column>
+                    <template slot-scope="scope">
+                        <router-link :to="{ name: 'Paciente', params: { id: scope.row.id } }">Ver</router-link>
+                    </template>
+                </el-table-column>
+            </el-table>
+            {{ dialogFormVisible }}
+            <el-dialog title="Shipping address" :visible.sync="dialogFormVisible">
+                <el-form :model="newEntry" label-width="120px">
+                    <el-form-item label="Promotion name">
+                        <el-input v-model="newEntry.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Zones">
+                        <el-select v-model="newEntry.region" style="width: 100%;">
+                            <el-option label="Zone No.1" value="shanghai"></el-option>
+                            <el-option label="Zone No.2" value="beijing"></el-option>
+                        </el-select>
+                    </el-form-item>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                    <el-button @click="dialogFormVisible = false">Cancel</el-button>
+                    <el-button type="primary" @click="dialogFormVisible = false">Confirm</el-button>
+                </span>
+            </el-dialog>
+        </el-main>
+        <div class="comment-frame">
+            <div class="comment-box">
+                <textarea class="comment-box-input js-new-comment-input" placeholder="Escriba un comentario..." dir="auto" style="overflow: hidden; overflow-wrap: break-word; height: 79px;"></textarea>
+                <div class="comment-box-options">
+                    <a class="comment-box-options-item js-comment-add-attachment" href="#" title="Añadir un adjunto...">
+                        <span class="icon-sm icon-attachment"></span>
+                    </a>
+                    <a class="comment-box-options-item js-comment-mention-member" href="#" title="Designar a un miembro…">
+                        <span class="icon-sm icon-mention"></span>
+                    </a>
+                    <a class="comment-box-options-item js-comment-add-emoji" href="#" title="Añadir un emoticono...">
+                        <span class="icon-sm icon-emoji"></span>
+                    </a><a class="comment-box-options-item js-comment-add-card" href="#" title="Añadir tarjeta...">
+                        <span class="icon-sm icon-card"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-  name: "Clinica",
-  data() {
-    return {
-			dialogFormVisible: false,
-			newEntry: {
-				name: "",
-				region: ""
-			},
-      clinica: {
-        id: 1,
-        name: "casa",
-        beds_available: 100,
-				permission: "cualca",
-        pacientes: [
-        	{
-        		id: 1,
-        		name: "Carlos",
-        		last_name: "Gomez",
-        		dni: "12.134.565",
-        		numero_de_paciente: "0012-19"
-        	}
-        ]
-      }
+    name: "Clinica",
+    data() {
+        return {
+            dialogFormVisible: false,
+            newEntry: {
+                name: "",
+                region: ""
+            },
+            clinica: {
+                id: 1,
+                name: "casa",
+                beds_available: 100,
+                permission: "cualca",
+                pacientes: [{
+                    id: 1,
+                    name: "Carlos",
+                    last_name: "Gomez",
+                    dni: "12.134.565",
+                    numero_de_paciente: "0012-19"
+                }]
+            }
+        }
+    },
+    methods: {
+        openModal() {
+            this.dialogFormVisible = true;
+        }
     }
-	},
-	methods: {
-		openModal() {
-			this.dialogFormVisible = true;
-		}
-	}
 };
 </script>
+
+<style>
+	.comment-frame {
+		border-radius: 3px;
+		margin: 4px 4px 12px 0;
+	}
+
+
+
+</style>
--- src/components/clinicas/nuevoPaciente.vue
+++ src/components/clinicas/nuevoPaciente.vue
@@ -1,28 +1,39 @@
 <template>
-	<div class="container-wrapper">
-		<el-header>
-			<div class="main-title">Nuevo Paciente</div>
-		</el-header>
-		<el-main>
-			<el-form>
-				<el-form-item label="Nombre">
-					<el-input placeholder="nombre" v-model="input1"/>
-				</el-form-item>
-				<el-form-item label="Apellido">
-					<el-input placeholder="apellido" v-model="input2"/>
-				</el-form-item>
-				<el-form-item label="Dni">
-					<el-input placeholder="dni" v-model="input3"/>
-				</el-form-item>
-				<el-form-item label="Numero de paciente">
-					<el-input placeholder="numero de paciente" v-model="input4"/>
-				</el-form-item>
-				<el-form-item>
-					<el-button>Guardar</el-button>
-				</el-form-item>
-			</el-form>
-		</el-main>
-	</div>
+    <div class="container-wrapper">
+        <el-header>
+            <div class="main-title">Nuevo Paciente</div>
+        </el-header>
+        <el-main>
+            <el-form>
+                <el-form-item label="Nombre">
+                    <el-input placeholder="nombre" v-model="input1" />
+                </el-form-item>
+                <el-form-item label="Apellido">
+                    <el-input placeholder="apellido" v-model="input2" />
+                </el-form-item>
+                <el-form-item label="Dni">
+                    <el-input placeholder="dni" v-model="input3" />
+                </el-form-item>
+                <el-form-item label="Numero de paciente">
+                    <el-input placeholder="numero de paciente" v-model="input4" />
+                </el-form-item>
+                <el-form-item>
+                    <el-button>Guardar</el-button>
+                </el-form-item>
+            </el-form>
+        </el-main>
+        <el-input type="textarea" :rows="4" placeholder="informe del paciente" v-model="textarea">
+        </el-input>
+        <el-cascader :options="options" clearable>tipo de internacion</el-cascader>
+        <div class="block">
+            <span class="demonstration">fecha de entrada</span>
+            <el-date-picker v-model="value1" type="date" placeholder="fecha">
+            </el-date-picker>
+        </div>
+        <br>
+        <br>
+        <br>
+    </div>
 </template>
 <script>
 export default {
@@ -31,7 +42,24 @@ export default {
             input1: '',
             input2: '',
             input3: '',
-            input4: ''
+            input4: '',
+            textarea: '',
+            value1: '',
+            options: [{
+                value: 'voluntario',
+                label: 'judicial',
+                pickerOptions: {
+                    disabledDate(time) {
+                        return time.getTime() > Date.now();
+                    },
+                    shortcuts: [{
+                        text: 'Today',
+                        onClick(picker) {
+                            picker.$emit('pick', new Date());
+                        }
+                    }, ]
+                }
+            }]
         }
     }
 };
